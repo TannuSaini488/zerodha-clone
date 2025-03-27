9d8b5360cfd990c69fd3ba5ae8ba145d7505a9cf
Tannu Saini <tannu2005saini@gmail.com>
2025-03-27 11:51:32 +0530

    final setup

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://zerodha-clone-ochre-nine.vercel.app/newOrder", {
+    axios.post("http://localhost:3002/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
