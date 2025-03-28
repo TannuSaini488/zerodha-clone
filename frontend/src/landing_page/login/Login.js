0e54c457660e6171190765f1340ea182ce82032f
Tannu Saini <tannu2005saini@gmail.com>
2025-03-28 20:42:50 +0530

    URL Change

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -4,31 +4,84 @@ import { useState } from "react";
 import axios from "axios";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [userExistance, setUserExistance] = useState(true);
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [userExistance, setUserExistance] = useState(true);
 
-  const handleSubmit = (event) => {
-    axios
-      .post(`${process.env.REACT_APP_BACKEND_URL}/login`, { email, password })
-      .then((res) => {
-        //checks the status code. if 201, user is new and brings user in dashboard
-        if (res.status === 201) {
-          setTimeout(() => {
-            window.location.href = process.env.REACT_APP_DASHBOARD_URL;
-          }, 500);
-          //if 202, shows user a pop up, that the user exist & stays the user in signup page
-        } else if (res.status === 202) {
-          setUserExistance(false);
-        }
-      });
-    event.preventDefault();
-    setEmail("");
-    setPassword("");
+  // const handleSubmit = (event) => {
+  //   axios
+  //     .post(`${process.env.REACT_APP_BACKEND_URL}/login`, { email, password })
+  //     .then((res) => {
+  //       //checks the status code. if 201, user is new and brings user in dashboard
+  //       if (res.status === 201) {
+  //         setTimeout(() => {
+  //           window.location.href = process.env.REACT_APP_DASHBOARD_URL;
+  //         }, 500);
+  //         //if 202, shows user a pop up, that the user exist & stays the user in signup page
+  //       } else if (res.status === 202) {
+  //         setUserExistance(false);
+  //       }
+  //     });
+  //   event.preventDefault();
+  //   setEmail("");
+  //   setPassword("");
+  // };
+
+  // const closePopup = () => {
+  //   setUserExistance(true);
+  // };
+
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputValue;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
   };
 
-  const closePopup = () => {
-    setUserExistance(true);
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-left",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+       `${process.env.REACT_APP_BACKEND_URL}/login`,
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      console.log(data);
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+    });
   };
 
   return (
