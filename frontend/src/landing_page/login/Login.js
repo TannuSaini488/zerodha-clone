bd5310bcc5d9b6e65063abf6b52325e9fd3d4390
Tannu Saini <tannu2005saini@gmail.com>
2025-03-28 20:05:52 +0530

    URL change

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -10,7 +10,7 @@ function Login() {
 
   const handleSubmit = (event) => {
     axios
-      .post(`${process.env.REACT_APP_BACKEND_URL}/logIn`, { email, password })
+      .post(`${process.env.REACT_APP_BACKEND_URL}/login`, { email, password })
       .then((res) => {
         //checks the status code. if 201, user is new and brings user in dashboard
         if (res.status === 201) {
