325c1fe0272fdac86ad75654b8f1fbc9bc223e8d
Tannu Saini <tannu2005saini@gmail.com>
2025-03-30 18:55:33 +0530

    final setup

--- backend/index.js
+++ backend/index.js
@@ -204,7 +204,11 @@ app.get("/allHoldings", async (req, res) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3002", "https://zerodha-clone-h331.vercel.app"],
+    origin: [
+      "http://localhost:3002",
+      "https://zerodha-clone-tryb.vercel.app",
+      "https://zerodha-clone-v9rc.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
