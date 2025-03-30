83f4d1300de963e01dd905c51fdd1c4b35d4edcf
Tannu Saini <tannu2005saini@gmail.com>
2025-03-30 21:18:42 +0530

    finalsetup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,7 +18,7 @@ import HomePage from "./landing_page/home/HomePage.js";
 export {default as Login} from './landing_page/login/Login.js';
 export {default as Signup} from './landing_page/signup/Signup.js';
 
-Navigate(`${process.env.REACT_APP_DASHBOARD_URL}`);
+// Navigate(`${process.env.REACT_APP_DASHBOARD_URL}`);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
